94773982fac24f3c2447a01ef98d0c6dd0f8d356
Yer Yang <yyhmg_86@yahoo.com>
2023-09-27 17:10:14 -0700

    renders random image of a dog to the DOM

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
+import React, { useEffect, useState } from "react";
 // create your App component here
+
+function App() {
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(resp => resp.json())
+            .then(data => setImage(data.message))
+    }, [])
+
+    if (!image) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <>
+            <img src={image} alt="A Random Dog"></img>
+        </>
+    )
+}
+
+export default App
